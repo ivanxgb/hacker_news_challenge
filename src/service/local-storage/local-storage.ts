6a2699587ba972987cdc9d07abe2d878e7502684
ivanxgb <ivanxgb@gmail.com>
2023-05-18 20:00:47 -0500

    ref!: `LocalStorage` class, created field `_savedNews` and reduced calls made to `local storage` api.

--- src/service/local-storage/local-storage.ts
+++ src/service/local-storage/local-storage.ts
@@ -4,56 +4,63 @@ import { HitsModel } from "@/service/models/HitsModel";
 export class LocalStorage {
   private static instance: LocalStorage;
 
-  private constructor(private readonly key: string) {}
+  private readonly _savedNews: HitsModel[] = [];
+
+  private constructor(private readonly key: string) {
+    this._savedNews = this.getAllFromLocalStorage();
+  }
+
+  // getter for saved news
+  public get savedNews(): HitsModel[] {
+    return this._savedNews;
+  }
 
   // get instance of LocalStorage using singleton pattern
   public static getInstance(key: string): LocalStorage {
     if (!LocalStorage.instance) {
       LocalStorage.instance = new LocalStorage(key);
     }
+
     return LocalStorage.instance;
-  }
-
-  // get all items from local storage or empty array if there is no items
-  public getAll(): HitsModel[] {
-    const saved = localStorage.getItem(this.key);
-    if (saved) {
-      return JSON.parse(saved) as HitsModel[];
-    }
-
-    return [];
   }
 
   // save news to local storage
   public saveNews(news: HitsModel): void {
-    const items = this.getAll();
-    items.push(news);
-    this.saveAll(items);
+    this._savedNews.push(news);
+    this.saveAll();
   }
 
   // check if news is already saved in local storage by comparing story_id and story_title
   // and return true if it is already saved
   public hasNews(news: HitsModel): boolean {
-    const allNews = this.getAll();
-    return allNews.some(
+    return this._savedNews.some(
       (n) => n.story_id === news.story_id && n.story_title === news.story_title,
     );
   }
 
   // remove news from local storage by comparing story_id and story_title
   public removeNews(news: HitsModel): void {
-    const allNews = this.getAll();
-    const indexNews = allNews.findIndex(
+    const indexNews = this._savedNews.findIndex(
       (n) => n.story_id === news.story_id && n.story_title === news.story_title,
     );
     if (indexNews !== -1) {
-      allNews.splice(indexNews, 1);
-      this.saveAll(allNews);
+      this._savedNews.splice(indexNews, 1);
+      this.saveAll();
     }
   }
 
   // save all news to local storage
-  private saveAll(news: HitsModel[]): void {
-    localStorage.setItem(this.key, JSON.stringify(news));
+  private saveAll(): void {
+    localStorage.setItem(this.key, JSON.stringify(this._savedNews));
+  }
+
+  private getAllFromLocalStorage(): HitsModel[] {
+    if (typeof window !== "undefined") {
+      const savedNews = localStorage.getItem(this.key);
+      if (savedNews) {
+        return JSON.parse(savedNews);
+      }
+    }
+    return [];
   }
 }
